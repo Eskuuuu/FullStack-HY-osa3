4df290eaceb6dadc8dd5d888d72a6c72427555c6
Eskuuuu <esa.iivonen@aalto.fi>
2024-12-31 00:18:50 +0200

    errorhandlingiä lisätty mutta uri ei toimi

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
+require('dotenv').config()
+console.log('MONGO_URI:', process.env.MONGO_URI)
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const app = express()
-
-require('dotenv').config()
+const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('dist'))
-
-const cors = require('cors')
 app.use(cors())
-const Person = require('./models/person')
-
-
-
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
-
-const Person  = require('./models/person')
+app.use(requestLogger)
 
 app.get('/api/persons', (request, response) => {
   Person.find({})
@@ -48,18 +20,24 @@ app.get('/api/persons', (request, response) => {
 })
 
   
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-    .then( person => {
+    .then(person => {
+      if (person) {
       response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-})
+    .catch(error => next(error))
+  })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })  
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -91,6 +69,22 @@ app.get('/info', (request, response) => {
 `;
   response.send(message)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
